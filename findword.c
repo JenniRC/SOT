525813ee5a533e42c5e3acf5c2454b3afacff2cf
JenniRC <jenniteleco@gmail.com>
2014-11-10 19:09:53 +0100

    Update findword.c

--- findword.c
+++ findword.c
@@ -34,7 +34,7 @@ built_path(char *s1, char *path){
 
 	n1 = strlen(s1);
 	n2 = strlen(path);
-	aux = malloc (n1+n2+2); // uno mas por la barra y otro por el /0
+	aux = malloc (n1+n2+2);
 	sprintf(aux, "%s/%s", path, s1);
 	return aux;
 }
@@ -50,39 +50,25 @@ search_in_dir(char* dir,char* argv){
 	//	err(1, dir);
 	//}
 	char *buf =malloc(sizeof (PATH_MAX));
-	printf("Empezamos a analizar %s \n",dir);
 	while((de = readdir(d)) != NULL) {
-		//	char *name = &de->d_name[0];
 			buf= built_path(de->d_name,dir);
-			//printf("tenemos el path :%s\n",buf);
-
-		//	if((strcmp(buf, ".") != 0) && (strcmp(buf, "..")!=0)){
 			if (is_file(buf)){
 				myFile = fopen(buf, "r");
 				char firstword [20];
 				fscanf(myFile,"%s ",firstword);
 					if(strcmp(firstword,argv)==0){
-						printf("\t%s Es fichero con texto buscado\n", buf);
-						//printf("%s\n", buf); NECESARIO
-					}
-					else{
-						printf("\t%s NO es fichero con texto buscado\n", buf);
+						printf("%s\n", buf);
 					}
 			}else{
-
 				if(is_dir(buf)){
 					if((strcmp(de->d_name, ".") != 0) && (strcmp(de->d_name, "..")!=0)){
-//						buf = built_path(de->d_name,dir);
-						printf("\t%s Es directorio\n",buf);
 						search_in_dir(buf,argv);
 					}
 				}else{
-					printf("\t%s Ni dir ni file\n", buf);
+					printf("\t%s I does not a valid file or directory\n", buf);
 				}
 			}
-			//printf("Salimos del path :%s\n",buf);
 	}
-	printf("Hemos acabado de analizar el path %s\n", dir);
 	free(buf);
 	closedir(d);
 }
@@ -91,7 +77,14 @@ int
 main(int argc,char*argv[])
 {
 	if(argc==2){
-		search_in_dir("/home/jenni/Escritorio/prueba/prueba",argv[1]);
+		search_in_dir(".",argv[1]);
+	}
+	int i;
+	if (argc > 2){
+		for (i = 1; i < argc - 1; i++){
+			search_in_dir(argv[i+1],argv[1]);
+			//printf("entro %d veces con %s \n", i,argv[i+1]);
+		}	
 	}
 	exit(0);
 }
